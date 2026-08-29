eab24fe75b2732fd61d5db6d2f41118d2ad0f80c
Trailee <178453713+trailee@users.noreply.github.com>
2026-08-29 17:17:04 -0400

    collisions + delete activities inter

--- bum-maxxing/background.gd
+++ bum-maxxing/background.gd
@@ -1,12 +1,11 @@
 extends Node2D
-@onready var activities: Node2D = $ui/HBoxContainer/activities_interface
+
 @onready var bumplace: Node2D = $ui/HBoxContainer/bumplace_interface
 
 @export var textures: Array[Texture2D] = []
 @onready var sprite: Sprite2D = $Sprite2D
 
 func _ready():
-	activities.hide()
 	bumplace.hide()
 
 func _on_static_body_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
