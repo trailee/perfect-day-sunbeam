1f7a9f627b2a365cfc8c33e99449a2a2ab9ed38a
Trailee <178453713+trailee@users.noreply.github.com>
2026-08-29 17:08:56 -0400

    phone

--- bum-maxxing/interface/bumplace_interface.gd
+++ bum-maxxing/interface/bumplace_interface.gd
@@ -1,13 +1,65 @@
 extends Node2D
-const activities = preload("uid://bk4y2vqcxuvl7")
+@onready var activities: Control = $"activities UI"
+@onready var bumplace: Control = $"bumplace UI"
+@onready var tv_buy: Control = $"tv buy UI"
+@onready var drinking_buy: Control = $"drinking buy UI"
+@onready var eating_buy: Control = $"eating buy UI"
+@onready var nap_buy: Control = $"nap buy UI"
+@onready var scroll_buy: Control = $"scroll buy UI"
+
+func _ready():
+	show()
+	bumplace.show()
+	activities.hide()
+	tv_buy.hide()
+	drinking_buy.hide()
+	eating_buy.hide()
+	nap_buy.hide()
+	scroll_buy.hide()
+	
 func _on_activ_pressed() -> void:
-	hide()
+	bumplace.hide()
 	activities.show()
 
-
 func _on_access_pressed() -> void:
-	hide()
-
+	bumplace.hide()
 
 func _on_button_pressed() -> void:
-	hide()
+	if bumplace.visible:
+		hide()
+	elif activities.visible:
+		activities.hide()
+		bumplace.show()
+	else:
+		tv_buy.hide()
+		drinking_buy.hide()
+		eating_buy.hide()
+		nap_buy.hide()
+		scroll_buy.hide()
+		activities.show()
+
+
+# BUTTONS
+func _on_tv_pressed() -> void:
+	activities.hide()
+	tv_buy.show()
+
+
+func _on_bev_pressed() -> void:
+	activities.hide()
+	drinking_buy.show()
+
+
+func _on_muk_pressed() -> void:
+	activities.hide()
+	eating_buy.show()
+
+
+func _on_sleep_pressed() -> void:
+	activities.hide()
+	nap_buy.show()
+
+
+func _on_scroll_pressed() -> void:
+	activities.hide()
+	scroll_buy.show()
